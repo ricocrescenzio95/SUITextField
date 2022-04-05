550b2366c62cf9bf6ab8941a812eb57c47d4aa1c
Rico Crescenzio <rico.crescenzio@gmail.com>
2022-04-05 17:52:25 +0200

    Removed useless package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
             targets: ["SwiftUITextField"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
