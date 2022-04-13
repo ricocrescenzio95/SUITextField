85905bc3bcab0c610951ed7b2e041de681804192
Rico Crescenzio <rico.crescenzio@gmail.com>
2022-04-13 12:29:51 +0200

    Reset the example

--- SUITextFieldExample/SUITextFieldExample/ContentView.swift
+++ SUITextFieldExample/SUITextFieldExample/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 import SwiftUITextField
 
 struct ContentView: View {
-
+    
     enum Responder: CaseIterable {
         case first
         case second
         case third
         case fourth
     }
-
+    
     @State private var toggleFont = false
     @State private var text = "A test text"
     @ResponderState var focus: Responder?
@@ -24,41 +24,99 @@ struct ContentView: View {
     @State private var toggle = false
     @State private var date = Date()
     @State private var myDouble: Double? = 0.0
-
+    
     let attributes: [NSAttributedString.Key: Any] = [
         .kern: 5,
         .foregroundColor: UIColor.systemOrange
     ]
-
+    
     let placeholderAttributes: [NSAttributedString.Key: Any] = [
         .kern: 5,
         .foregroundColor: UIColor.systemGray
     ]
-
+    
     var body: some View {
-      VStack {
-        SUITextField(text: $text, placeholder: "Hello")
-          .onReturnKeyPressed { isFocused = false }
-          .responder($isFocused)
-      }
-      .uiTextFieldTextAlignment(.right)
-      .uiTextFieldBorderStyle(.roundedRect)
+        NavigationView {
+            ScrollView {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 100)
+                    .padding(.vertical, 50)
+                VStack {
+                    SUITextField(text: $text)
+                        .inputAccessoryView {
+                            navigator
+                        }
+                        .onReturnKeyPressed {
+                            focus = nil
+                        }
+                        .leftView {
+                            Button(action: { text = "" }) {
+                                Image(systemName: "trash")
+                            }
+                            .padding(.horizontal, 2)
+                        }
+                        .responder($focus, equals: .first)
+                        .uiTextFieldTextLeftViewMode(.whileEditing)
+                    SUITextField(text: $text)
+                        .inputAccessoryView {
+                            navigator
+                        }
+                        .responder($focus, equals: .second)
+                    if #available(iOS 15, *) {
+                        SUITextField(
+                            value: $myDouble,
+                            format: .number,
+                            placeholder: AttributedString("Hey there").mergingAttributes(AttributeContainer(placeholderAttributes))
+                        )
+                        .inputAccessoryView {
+                            navigator
+                        }
+                        .responder($focus, equals: .third)
+                        SUITextField(value: $date, format: .dateTime)
+                            .inputAccessoryView {
+                                navigator
+                            }
+                            .inputView {
+                                datePicker
+                            }
+                            .responder($focus, equals: .fourth)
+                    } else {
+                        SUITextField(text: .constant(date.description))
+                            .inputAccessoryView {
+                                navigator
+                            }
+                            .inputView {
+                                datePicker
+                            }
+                            .responder($focus, equals: .third)
+                    }
+                }
+                .padding()
+            }
+            .uiTextFieldDefaultTextAttributes(attributes)
+            .uiTextFieldFont(toggleFont ? .monospacedSystemFont(ofSize: 50, weight: .medium) : nil)
+            .uiTextFieldBorderStyle(.roundedRect)
+            .uiTextFieldAdjustsFontSizeToFitWidth(.enabled(minSize: 8))
+            .navigationBarTitle("SUITextField")
+        }
     }
-
+    
     private var navigator: some View {
         ResponderNavigatorView(responder: $focus)
             .centerView {
                 Text(text)
             }
     }
-
+    
     private var datePicker: some View {
         DatePicker("Select date", selection: $date)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .datePickerStyle(.wheel)
             .labelsHidden()
     }
-
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
